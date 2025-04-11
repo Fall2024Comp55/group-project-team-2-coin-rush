d6dd9d3809d198f8f844b07aaaef80e99db52e6d
TYTengYuan <t_yuan2@u.pacific.edu>
2025-04-11 16:11:54 -0700

    Added a button with "START LEVEL 1" label using GCompound

--- src/WelcomePane.java
+++ src/WelcomePane.java
@@ -14,9 +14,9 @@ public class WelcomePane extends GraphicsPane{
 	@Override
 	public void showContent() {
 		addPicture();
-		addDescriptionButton();
-		addPicture1();
-		addPicture2();
+		AddButton();
+		AddButton1();
+		AddButton2();
 	}
 
 	@Override
@@ -37,7 +37,7 @@ public class WelcomePane extends GraphicsPane{
 	}
 	
 	// Add a button with "START" label using GCompound
-	private void addDescriptionButton() {
+	private void AddButton() {
 		GImage moreButton = new GImage("CGB02-yellow_L_btn.png", 200, 400);
 		moreButton.scale(0.3, 0.3);
 		
@@ -71,16 +71,30 @@ public class WelcomePane extends GraphicsPane{
 
 	}
 	
-	private void addPicture1() {
+	private void AddButton1() {
 		GImage moreButton1 = new GImage("CGB02-yellow_L_btn.png", 200, 400);
 		moreButton1.scale(0.3, 0.3);
-		moreButton1.setLocation((mainScreen.getWidth() - moreButton1.getWidth())/ 2, 450);
 		
-		contents.add(moreButton1);
-		mainScreen.add(moreButton1);
+		GLabel buttonLabel = new GLabel("START LEVEL 1");
+		buttonLabel.setFont("SansSerif-bold-18");
+		buttonLabel.setColor(Color.WHITE);
+		
+		GCompound buttonCompound = new GCompound();
+		buttonCompound.add(moreButton1, 0, 0);
+		
+		double labelX = (moreButton1.getWidth() - buttonLabel.getWidth()) / 2;
+		double labelY = (moreButton1.getHeight() + buttonLabel.getAscent()) / 2;
+		buttonCompound.add(buttonLabel, labelX, labelY);
+		
+		double x =  (mainScreen.getWidth() - buttonCompound.getWidth()) / 2;
+		double y = 450;
+		buttonCompound.setLocation(x, y);
+		
+		contents.add(buttonCompound);
+		mainScreen.add(buttonCompound);
 	}
 	
-	private void addPicture2() {
+	private void AddButton2() {
 		GImage moreButton2 = new GImage("CGB02-yellow_L_btn.png", 200, 400);
 		moreButton2.scale(0.3, 0.3);
 		moreButton2.setLocation((mainScreen.getWidth() - moreButton2.getWidth())/ 2, 500);
