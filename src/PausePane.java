2a861690e9642dae5533faf01aa05d60903ed9e4
TYTengYuan <t_yuan2@u.pacific.edu>
2025-04-18 17:31:25 -0700

    Added a new image
    Created continue button

--- src/PausePane.java
+++ src/PausePane.java
@@ -1,3 +1,10 @@
+import java.awt.Color;
+
+import acm.graphics.GCompound;
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+import acm.graphics.GRect;
 
 public class PausePane extends GraphicsPane {
 	public PausePane(MainApplication mainScreen) {
@@ -5,5 +12,40 @@ public class PausePane extends GraphicsPane {
 	} 
 	
 	
+	public void showContent() {
+		createButton();
+		
+	}
+	
+	public void hideContent()  {
+		for(GObject item : contents) {
+			mainScreen.remove(item);
+		}
+		contents.clear();
+	}
+	
+	private void createButton() {
+		GImage continueButton = new GImage("CGB02-yellow_M_btn.png",200,500);
+		continueButton.scale(0.3, 0.3);
+		
+		GLabel continueLabel =  new GLabel("CONTINUE");
+		continueLabel.setFont("SansSerif-bold-18");
+		continueLabel.setColor(Color.WHITE);
+		
+		GCompound buttonCompound = new GCompound();
+		buttonCompound.add(continueButton, 0, 0);
+		
+		double labelX = (continueButton.getWidth() - continueLabel.getWidth()) / 2;
+		double labelY = (continueButton.getHeight() - continueLabel.getAscent()) / 2;
+		buttonCompound.add(continueLabel, labelX, labelY);
+		
+		double x = (mainScreen.getWidth() - buttonCompound.getWidth()) / 2;
+		double y = (mainScreen.getHeight() -buttonCompound.getHeight()) / 2;
+		
+		buttonCompound.setLocation(x, y);
+		
+		contents.add(buttonCompound);
+		mainScreen.add(buttonCompound);
+	}
 
 }
